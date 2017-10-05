3fde6da145c6206c42a3adc2cacf0c08f7defcbf
Sean McGroarty <mcgroas@tcd.ie>
2017-10-05 21:02:53 +0100

    Multiple clients can connect and will receive a message upon issuing the correct protocol

--- main.hs
+++ main.hs
@@ -3,10 +3,13 @@ module Main where
 import Network.Socket
 import System.IO
 import Data.List.Split
-import qualified Data.ByteString.Char8 as C
+import Control.Concurrent
 
-
-type Msg = (String)
+data User = User {
+		usr_id:: Int,
+		name:: String,
+		sock:: Socket
+	}
 
 main :: IO ()
 main = do
@@ -14,27 +17,33 @@ main = do
 	setSocketOption sock ReuseAddr	1
 	bind sock $ SockAddrInet 6969 iNADDR_ANY
 	listen sock 2
-	connLoop sock
+	connLoop sock 0
 
-connLoop :: Socket -> IO ()	
-connLoop sock = do
+connLoop :: Socket -> Int -> IO ()	
+connLoop sock userNum = do
 	conn <- accept sock
-	manageConn conn
-	connLoop sock
+	forkIO $ manageConn (fst conn) userNum
+	connLoop sock (userNum + 1)
 
-manageConn :: (Socket,SockAddr) -> IO ()
-manageConn (sock,_) = do
+manageConn :: Socket -> Int -> IO ()
+manageConn sock userNum = do
 	dInfo <- recv sock 4096
 	info <- return $ words dInfo
-	filterConn (head info) info sock
+	let user = User userNum "" sock 
+	filterConn (head info) user info
 	close sock
 
-filterConn :: String -> [String] -> Socket -> IO ()
-filterConn "JOIN_CHATROOM" info sock = joinChatroom info sock
-filterConn "LEAVE_CHATROOM" _ sock = leaveChatroom sock
+filterConn :: String -> User -> [String] -> IO ()
+filterConn "JOIN_CHATROOM" user info = joinChatroom user info
+filterConn "LEAVE_CHATROOM" user _ = leaveChatroom user
 filterConn s _ _ = killConn s
 
-joinChatroom _ _ = print "Join"
+joinChatroom user _ = do
+	let num = show (usr_id user)
+	send (sock user) ("Welcome " ++ num )
+	print ("User["++num++"] has joined.")
+	manageConn (sock user) (usr_id user)
+
 leaveChatroom _ = print "Leave"
 killConn error = do
 	 print error
