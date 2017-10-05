eafaee3a71d182a2f6fd6d1edeb9acc566ffc096
Sean McGroarty <mcgroas@tcd.ie>
2017-10-05 17:31:27 +0100

    Added manageConn and checkConn functions

--- main.hs
+++ main.hs
@@ -2,6 +2,9 @@ module Main where
 
 import Network.Socket
 import System.IO
+import Data.List.Split
+
+type Msg = (String)
 
 main :: IO ()
 main = do
@@ -9,4 +12,20 @@ main = do
 	setSocketOptions sock ReuseAddr	1
 	bind sock $ SockAddrInet 6969 iNADDR_ANY
 	listen sock 2
-	mainLoop sock	
+	connLoop sockn
+
+connLoop :: Socket -> IO ()	
+connLoop sock = do
+	conn <- accept sock
+	manageConn conn
+	connLoop sock
+
+manageConn :: (Socket,SocketAddr) -> IO ()
+manageConn (sock,_) = do
+	info <- recv sock
+	checkConn $ unpack info
+	close sock
+
+checkConn :: Char -> IO ()
+checkConn info = do
+	splitInfo = map (splitOn ":") $ splitOn "\n" info
