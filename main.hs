cc8556e144882dadb54e619f43a2bac61c0509f8
Sean McGroarty <mcgroas@tcd.ie>
2017-10-05 19:11:07 +0100

    FilterConn working

--- main.hs
+++ main.hs
@@ -3,16 +3,18 @@ module Main where
 import Network.Socket
 import System.IO
 import Data.List.Split
+import qualified Data.ByteString.Char8 as C
+
 
 type Msg = (String)
 
 main :: IO ()
 main = do
 	sock <- socket AF_INET Stream 0
-	setSocketOptions sock ReuseAddr	1
+	setSocketOption sock ReuseAddr	1
 	bind sock $ SockAddrInet 6969 iNADDR_ANY
 	listen sock 2
-	connLoop sockn
+	connLoop sock
 
 connLoop :: Socket -> IO ()	
 connLoop sock = do
@@ -20,12 +22,19 @@ connLoop sock = do
 	manageConn conn
 	connLoop sock
 
-manageConn :: (Socket,SocketAddr) -> IO ()
+manageConn :: (Socket,SockAddr) -> IO ()
 manageConn (sock,_) = do
-	info <- recv sock
-	checkConn $ unpack info
+	dInfo <- recv sock 4096
+	info <- return $ words dInfo
+	filterConn (head info) info sock
 	close sock
 
-checkConn :: Char -> IO ()
-checkConn info = do
-	splitInfo = map (splitOn ":") $ splitOn "\n" info
+filterConn :: String -> [String] -> Socket -> IO ()
+filterConn "JOIN_CHATROOM" info sock = joinChatroom info sock
+filterConn "LEAVE_CHATROOM" _ sock = leaveChatroom sock
+filterConn s _ _ = killConn s
+
+joinChatroom _ _ = print "Join"
+leaveChatroom _ = print "Leave"
+killConn error = do
+	 print error
