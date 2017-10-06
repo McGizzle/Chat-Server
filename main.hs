6a9a9e96f442bc3415a7132401b2c6393508c497
Sean McGroarty <mcgroas@tcd.ie>
2017-10-06 20:46:28 +0100

    manageConn reads from the socket

--- main.hs
+++ main.hs
@@ -3,7 +3,12 @@ module Main where
 import Network.Socket
 import System.IO
 import Control.Concurrent
+import Control.Concurrent.STM
 import Control.Monad
+import qualified Data.HashTable.IO as H
+
+type HashTable k v = H.BasicHashTable k v
+type Chat_HT = TVar (HashTable String (Chan String)) 
 
 data User = User {
 		usr_id:: Int,
@@ -17,45 +22,54 @@ main = do
 	setSocketOption sock ReuseAddr	1
 	bind sock $ SockAddrInet 6969 iNADDR_ANY
 	listen sock 2
-	connLoop sock 0
+	ht <- newTVarIO ([] :: [String, Chan String])
+	connLoop sock 0 ht
 
 ----Connection Functions---------
-connLoop :: Socket -> Int -> IO ()	
-connLoop sock userNum = do
+connLoop :: Socket -> Int -> TVar [String] -> IO ()	
+connLoop sock userNum ht = do
 	conn <- accept sock
-	forkIO $ manageConn (fst conn) userNum
-	connLoop sock (userNum + 1)
+	forkIO $ manageConn (fst conn) userNum ht
+	connLoop sock (userNum + 1) ht
 
-manageConn :: Socket -> Int -> IO ()
-manageConn sock userNum = do
+manageConn :: Socket -> Int -> TVar [String] -> IO ()
+manageConn sock userNum ht = do
+--	hdl <- socketToHandle (sock user) ReadWriteMode
+--	hSetBuffering hdl NoBuffering
+--	msg <- fmap init (hGetLine hdl)
 	dInfo <- recv sock 4096
-	info <- return $ words dInfo
-	let user = createUser userNum "" sock 
-	filterConn (head info) user info
+	info <- return $ words msg
+	let user = User userNum "" sock 
+	filterConn (head info) user info ht
 	close sock
 
-filterConn :: String -> User -> [String] -> IO ()
-filterConn "Join" user info = joinChatroom user info
-filterConn "LEAVE_CHATROOM" user _ = leaveChatroom user
-filterConn s _ _ = killConn s
+
+filterConn :: String -> User -> [String] -> TVar [String] -> IO ()
+filterConn "Join" user info ht = joinChatroom user info ht
+filterConn "LEAVE_CHATROOM" user _ _ = leaveChatroom user
+filterConn s _ _ _ = killConn s
 ----------------------------------
 
 ---CHATROOM FUNCTIONS-------------
-joinChatroom user info = do
+initChatroom :: String -> TVar [String] -> Chan String
+initChatroom name ht = do
+	
+
+joinChatroom user info ht = do
 	let num = show (usr_id user)
 	send (sock user) ("Welcome " ++ num ++ "\n" )
 	print ("User["++num++"] has joined.")
-	chatroom <- initChatroom
+	chName <- return getChatroomName
+	chatroom <- initChatroom chName ht
 	chat chatroom user	
 
-initChatroom :: IO (Chan String)
-initChatroom = newChan
+	
 
 leaveChatroom _ = print "Leave"
 killConn error = print error
 
-getChatroomName :: [String] -> String
-getChatroomName _ = "Ch_room_1"
+getChatroomName :: String
+getChatroomName = "Ch_room_1"
 
 chat :: Chan String -> User -> IO ()
 chat chatroom user = do
@@ -73,9 +87,6 @@ chat chatroom user = do
 ----------------------------------
 
 ---USER FUNCTIONS-----------------
-createUser :: Int -> String -> Socket -> User
-createUser usr_id name sock = User usr_id name sock
-
 updateName :: String -> User -> User
 updateName new_name user = User (usr_id user) new_name (sock user)
 ----------------------------------
