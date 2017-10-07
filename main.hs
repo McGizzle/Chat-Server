49c4b7984c8f781fb6ced98646098757ecc4b481
Sean McGroarty <mcgroas@tcd.ie>
2017-10-07 17:18:05 +0100

    Added debugging info on both client and server side. Chatroom functionality is working.

--- main.hs
+++ main.hs
@@ -4,10 +4,14 @@ import System.IO
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Monad
+import Control.Monad (replicateM)
 import Data.Maybe
 
-type Chatroom = TVar [(String, Chan String)]
+type Chatroom = TVar [(String, Chan Msg)]
 data User = User { name :: String, usr_id :: Int, hdl :: Handle }
+ deriving Show
+
+type Msg = (String, Int)
 
 main :: IO ()
 main = do
@@ -15,38 +19,49 @@ main = do
   setSocketOption sock ReuseAddr 1
   bind sock $ SockAddrInet 6969 iNADDR_ANY
   listen sock 2
-  chan <- newChan
-  let lobby = ("lobby",chan) : []
   chats <- atomically $ newTVar []
   connLoop sock chats 0
 
 connLoop :: Socket -> Chatroom -> Int -> IO ()
 connLoop sock chats num = do
   conn <- accept sock
-  hdl <- socketToHandle (fst conn) ReadWriteMode
-  hSetBuffering hdl NoBuffering
-  dInfo <- fmap init (hGetContents hdl)
-  info <- return $ words dInfo	
-  let usr = User "" num hdl
-  chName <- fmap init (hGetLine hdl)
-  chan <- getChannel chats chName
-  forkIO $ runChat chan usr
+  forkIO $ manageConn (fst conn) chats num
   connLoop sock chats (num + 1)
+
+manageConn :: Socket -> Chatroom -> Int -> IO ()
+manageConn sock chats num = do
+  print ("User[" ++ show num ++ "] has joined the network.")
+  hdl <- socketToHandle sock ReadWriteMode
+  hSetBuffering hdl NoBuffering
+  dInfo <- fmap init (hGetLine hdl)
+  info <- return $ words dInfo
+  let usr = User "" num hdl
+  let chName = head info
+  chan <- getChannel chats chName
+  runChat chan usr
+
+hGetLines :: Int -> Handle -> IO [String]
+hGetLines n hdl = replicateM n (hGetLine hdl)
  
-runChat :: Chan String -> User -> IO ()
+runChat :: Chan (String,Int) -> User -> IO ()
 runChat chatroom usr = do
+  let sendMe msg = hPutStrLn (hdl usr) msg
+  let num = (usr_id usr)
+  let sendMsg msg = writeChan chatroom (msg,num) 
+  print ("User[" ++ show num ++ "] has joined a chatroom.")
   thisChat <- dupChan chatroom
-  
+  sendMsg ("---> ["++ show num ++"] has joined the chat.")   
+  sendMe ("Welcome to the chat.") 
+
   forkIO $ forever $ do
-    line <- readChan thisChat
-    hPutStrLn (hdl usr) line
-  
+    (line, msgNum) <- readChan thisChat
+    when (msgNum /= num) $ sendMe (line)
+ 
   forever $ do
     line <- fmap init (hGetLine (hdl usr))
-    writeChan chatroom line
+    sendMsg ("["++ show num  ++"]: " ++ line)
 
-
-getChannel :: Chatroom -> String -> IO (Chan String)
+getChannel :: Chatroom -> String -> IO (Chan Msg)
 getChannel chats name = do
 			  chan <- newChan
 			  atomically $ do list <- readTVar chats
@@ -58,7 +73,7 @@ getChannel chats name = do
 					    	   return chan
 					    else return $ fromJust result
 
-findCHR :: [(String, Chan String)] -> String -> Maybe (Chan String)
+findCHR :: [(String, Chan Msg)] -> String -> Maybe (Chan Msg)
 findCHR [] _ = Nothing
 findCHR (x:xs) name  
   | name == (fst x) = Just (snd x)
